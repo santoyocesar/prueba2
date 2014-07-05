fd5b8a049745ce847130a109fde123bd07d91ca4
santoyo <cesar.santoyo.romero@gmail.com>
2014-07-04 22:15:13 -0500

    actualizacion

--- polls/models.py
+++ polls/models.py
@@ -2,23 +2,25 @@ import datetime
 from django.utils import timezone
 from django.db import models
 
+
 class Poll(models.Model):
-	question = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('data published')
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('data published')
 
-	def __unicode__(self):
-		return self.question
+    def __unicode__(self):
+        return self.question
 
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
-	was_published_recently.admin_order_field='pub_date'
-	was_published_recently.boolean=True
-	was_published_recently.short_description='Published recently?'
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
-	poll = models.ForeignKey(Poll)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+    poll = models.ForeignKey(Poll)
+    hoice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-	def __unicode__(self):
-		return self.choice_text
+    def __unicode__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -3,48 +3,37 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 
-from polls.models import Poll,Choice
+from polls.models import Poll, Choice
 
-#def index(request):
-#	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-#	context = {'latest_poll_list':latest_poll_list}
-#	return render(request, 'polls/index.html', context)
-
-#def detail(request, poll_id):
-#	poll = get_object_or_404(Poll, pk=poll_id)
-#	return render(request,'polls/detail.html',{'poll':poll})
-
-#def results(request, poll_id):
-#	poll = get_object_or_404(Poll, pk=poll_id)
-#	return render(request,'polls/results.html',{'poll':poll})
-	#return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_poll_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-	def get_queryset(self):
-		""" Return the las five published polls. """
-		return Poll.objects.order_by('-pub_date')[:5]
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
-	model = Poll
-	template_name = 'polls/detail.html'
+    model = Poll
+    template_name = 'polls/detail.html'
 
-class ResultsView(generic.DetailView): 
-	model = Poll
-	template_name = 'polls/results.html'
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
 
 def vote(request, poll_id):
-	p = get_object_or_404(Poll, pk=poll_id)
-	try:
-		selected_choice = p.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html',{
-			'poll':p,
-			'error_message':"Elige una respuesta.",
-		})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "Elige una respuesta.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
